295572cd2b443eed63a15614a5f77a10ca94ce59
tbullmann <bullmann.torsten@gmail.com>
2019-08-26 12:50:16 +0200

    global parameters, stubs for other models

--- Model.py
+++ Model.py
@@ -8,22 +8,31 @@ import os
 current_path = os.getcwd()
 load_mechanisms(os.path.join(current_path, "mod"))
 
+# Parameters 
+myena = 55
+myek = -97
+nax_bouton = 1000 
+nax_axon = 0
+Kv1_bouton = 2000 
+Kv1_axon = 0
+leakK_bouton = 0.18 #.08
+leakK_axon = leakK_bouton/10
+leakNa_bouton = leakK_bouton/13
+leakNa_axon = leakNa_bouton/10
+
+# Global hoc variables
+h.vShift_Kv1 = 25
+h.vShift_inact_Kv1 = -15
+h.speed_Kv1 = 2.3
+h.vShift_nax = 20
+h.vShift_inact_nax = 10
+h.q10_nax = 4
+h.q10h_nax = 4
+
 
 def without_ih():
+    # Model with standard potassium und sodium currents
     
-    # Parameters 
-    myena = 55
-    myek = -97
-    nax_bouton = 1000 
-    nax_axon = 0
-    Kv1_bouton = 2000 
-    Kv1_axon = 0
-    leakK_bouton = 0.18 #.08
-    leakK_axon = leakK_bouton/10
-    leakNa_bouton = leakK_bouton/13
-    leakNa_axon = leakNa_bouton/10
-    
-    # Instantiate model
     model = AxonWithBoutons(axon_mechanisms  =[Mechanism('nax', gbar = nax_axon),
                                                Mechanism('Kv1', gbar = Kv1_axon),
                                                Mechanism('leakNa', g = leakNa_axon),
@@ -33,17 +42,29 @@ def without_ih():
                                                Mechanism('leakNa', g = leakNa_bouton),
                                                Mechanism('leakK', g = leakK_bouton),],
                             all_ena=myena, all_ek=myek
-                                      )
-    
-    # Global hoc variables
-    h.vShift_Kv1 = 25
-    h.vShift_inact_Kv1 = -15
-    h.speed_Kv1 = 2.3
-    h.vShift_nax = 20
-    h.vShift_inact_nax = 10
-    h.q10_nax = 4
-    h.q10h_nax = 4
-    
+                                      )    
     return model
 
 
+def with_ih():
+    # Model with ih current
+    
+    pass
+
+
+def with_ih_cAMP():
+    # Model with ih current increased by cAMP
+
+    pass
+
+
+def only_input():
+    # Effect of low membrane resistance only
+
+    pass
+
+
+def only_depol():
+    # Effect of depolarized membrane potential only
+
+    pass
